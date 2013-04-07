75c02dc20e2449866e645291924c1890a01cce58
aruhan <aruhanbura@gmail.com>
2013-04-08 00:58:13 +0900

    Canvas image manipulation, toDataURL

--- Scripts/_references.js
+++ Scripts/_references.js
@@ -1,2 +1,2 @@
-﻿/// <reference path="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.9.1.js" />
-/// <reference path="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.9.1-vsdoc.js" />
+﻿/// <reference path="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.9.1-vsdoc.js" />
+/// <reference path="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.9.1.js" />
